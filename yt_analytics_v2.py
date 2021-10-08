58e882c0c3ac93835821ed5316290cdfa8b81316
neelsrejan <neelsrejan@gmail.com>
2021-10-07 21:11:40 -0700

    making slow progress due to empty rows being returned for certain videos

--- yt_analytics_v2.py
+++ yt_analytics_v2.py
@@ -5,11 +5,24 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-SCOPES = ['https://www.googleapis.com/auth/yt-analytics.readonly']
+import json
+import requests
+import pandas as pd
+from datetime import date
+import request_arguments
 
-API_SERVICE_NAME = 'youtubeAnalytics'
-API_VERSION = 'v2'
+#SCOPES = ["https://www.googleapis.com/auth/yt-analytics.readonly"]
+SCOPES = ["https://www.googleapis.com/auth/youtube",
+        "https://www.googleapis.com/auth/youtube.readonly",
+        "https://www.googleapis.com/auth/yt-analytics.readonly",
+        "https://www.googleapis.com/auth/youtubepartner",
+        "https://www.googleapis.com/auth/yt-analytics-monetary.readonly"
+        ]
+
+API_SERVICE_NAME = "youtubeAnalytics"
+API_VERSION = "v2"
 CLIENT_SECRETS_FILE = os.path.join(os.getcwd(), "secrets.json")
+
 def get_service():
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
     credentials = flow.run_console()
@@ -22,18 +35,49 @@ def execute_api_request(client_library_function, **kwargs):
 
     print(response)
 
-if __name__ == '__main__':
-    # Disable OAuthlib's HTTPs verification when running locally.
-    # *DO NOT* leave this option enabled when running in production.
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    col_names = [col_header["name"] for col_header in response["columnHeaders"]]
+    data = response["rows"]
 
+    response_df = pd.DataFrame(data=data, columns=col_names)
+    print(response_df)
+
+def main():
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
     youtubeAnalytics = get_service()
+    
+    
+    API_KEY = input("Please enter your API KEY: ").strip()
+    channel_id = "UCWPXl--e3JsJxRG64Of_msA" #input("Please enter your channel id: ").strip()
+
+    num_vids = request_arguments.num_vids(channel_id, API_KEY)
+    vid_ids = request_arguments.get_vid_ids(num_vids, channel_id, API_KEY)
+    print(len(vid_ids))
+    print(vid_ids)
+
+    '''
+    today = str(date.today())
+    url = f"https://www.googleapis.com/youtube/v3/channels?part=snippet&id={channel_id}&key={API_KEY}"
+    date_response = json.loads(requests.get(url).text)["items"][0]["snippet"]["publishedAt"][:10]
+    print(today)
+    print(date_response)
+    '''
+    
     execute_api_request(
         youtubeAnalytics.reports().query,
-        ids='channel==MINE',
-        startDate='2021-09-01',
-        endDate='2021-10-01',
-        metrics='estimatedMinutesWatched,views,likes,subscribersGained',
-        dimensions='day',
-        sort='day'
-    )
+            dimensions="video,country",
+            endDate="2021-10-06",
+            #filters="video==wDAefGS4B2Q;country==US",
+            #filters="video==NaczzJYUCQc;country==US",
+            #filters="video==5by1jF54fEo;country==US",
+            filters="video==wDAefGS4B2Q,NaczzJYUCQc,5by1jF54fEo;country==US",
+            ids="channel==MINE",
+            #metrics="views,redViews",
+            metrics="views,redViews,comments,likes,dislikes,videosAddedToPlaylists,videosRemovedFromPlaylists,shares,estimatedMinutesWatched,estimatedRedMinutesWatched,averageViewDuration,averageViewPercentage,annotationClickThroughRate,annotationCloseRate,annotationImpressions,annotationClickableImpressions,annotationClosableImpressions,annotationClicks,annotationCloses,cardClickRate,cardTeaserClickRate,cardImpressions,cardTeaserImpressions,cardClicks,cardTeaserClicks,subscribersGained,subscribersLost",
+            startDate="2020-09-02"
+            )
+    
+
+if __name__ == "__main__":
+    main()
+    
+  
